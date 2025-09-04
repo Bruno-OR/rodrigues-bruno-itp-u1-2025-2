7da2cb4a9924e8a307f4fcf2d4b9e00f18875922
Bruno Rodrigues <brunocezar.eng@gmail.com>
2025-09-04 17:34:53 -0300

    Cálculo do Montante

---
+++ Atividades_Semanais/Semana_2/juros_compostos.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h> //Para incluir a função pow
+#include <locale.h>
+
+int main()
+{
+
+    setlocale(LC_CTYPE, "Portuguese"); // Usado para imprimir corretamente os acentos e caracteres especiais em português
+
+    float capital_inicial, taxa_juros, montante_final;
+    int tempo_anos;
+
+    printf("\nDigite o capital inicial: R$ ");
+    scanf("%f", &capital_inicial);
+    printf("Qual a taxa de juros? (Em %%): ");
+    scanf("%f", &taxa_juros);
+    printf("Agora digite o tempo de investimento em anos: ");
+    scanf("%d", &tempo_anos);
+
+    montante_final = capital_inicial * pow(1 + (taxa_juros / 100), tempo_anos);
+
+    printf("\n--- Resultado do Investimento ---\n");
+    printf("\nApós %d anos, o montante final será de: R$ %.2f", tempo_anos, montante_final);
+
+    return 0;
+}
