b85676d87c989c9cb4c35e109c1cf3c1d2383320
Bruno Rodrigues <brunocezar.eng@gmail.com>
2025-09-09 10:20:42 -0300

    Adição da questão 3 da lista 3

---
+++ Atividades_Semanais/Semana_3/notas.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+
+{
+    float nota1, nota2, nota3, media;
+
+    printf("\nDigite a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("\nDigite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("\nDigite a terceira nota: ");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("\n----------------RESULTADO----------------\n");
+
+    if (media >= 7)
+    {
+        printf("\nAluno Aprovado");
+    }
+    else if (media < 4)
+    {
+        printf("\nAluno Reprovado");
+    }
+    else
+    {
+        printf("\nAluno em Recuperação");
+    }
+
+    printf("\nMédia final: %.1f\n\n", media);
+    printf("----------------------------------------\n\n");
+
+    return 0;
+}
